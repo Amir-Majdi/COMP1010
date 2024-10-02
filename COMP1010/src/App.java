37fadb273346202280c940c8dca8a4adb48dc8a4
MattJButler <90148954+MattJButler@users.noreply.github.com>
2024-10-03 09:48:48 +1000

    Starting

---
+++ COMP1010/src/App.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class App {
+    public static void main(String[] args) {
+
+        Entity Player = new Entity();
+        Player.health = 1000;
+        System.out.print("Enter Your name: ");
+        Scanner sc = new Scanner(System.in);
+        Player.name = sc.nextLine();
+        Weapon
+        int rount
+
+        while (Player.isDead == false){
+            
+            for (int i = 1; i < 1000; i++){
+
+                for (int j = 1; j < 2; j++){
+
+                    for (int x = 1; x < 2; x++){
+
+                        
+                    }
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ COMP1010/src/Entity.java
@@ -0,0 +1,10 @@
+public class Entity {
+    String name;
+    public int health;
+    public String weapon;
+    int action;
+    public boolean isDead = false;
+
+
+
+}
